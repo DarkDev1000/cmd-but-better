8cbc9d6355feb712122a98d31e06949176ed100a
DarkDev1000 <112511290+DarkDev1000@users.noreply.github.com>
2023-05-01 00:44:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-## Import stuff
+# Import stuff
 import os
-import requests
-import colorama
-from colorama import Fore
+from prompt_toolkit import PromptSession
+from prompt_toolkit.lexers import PygmentsLexer
+from prompt_toolkit.styles import Style
+from pygments.lexers import PowerShellLexer
+from pygments.styles import get_style_by_name
 
-## Initialize colorama
-colorama.init(autoreset=True)
-
-## Welcome Message
+# Welcome message
 os.system("cls")
-
 os.system("title Command Prompt but Better")
-
 print("Welcome to Command Prompt but Better!")
 print("Made by DarkDev#2048")
 print("\n\n")
 
-## Main Code
+# Custom style for syntax highlighting
+style = Style.from_dict({
+    "prompt": "#1EAF50",
+    "output": ""
+})
+
+# Execute & highlight code
 def command_line():
+    session = PromptSession(lexer=PygmentsLexer(PowerShellLexer), style=style)
     while True:
-        command = input(Fore.BLUE + os.getcwd() + "$ " + Fore.GREEN)
-        print(Fore.WHITE)
-        if command.startswith("cd "):
-            directory = command[3:]
-            try:
-                os.chdir(directory)
-            except:
-                print("The system cannot find the path specified.")
-        else:
-            os.system(command)
-        print()
+        try:
+            command = session.prompt(os.getcwd() + "~"  + "$ ")
+            if command.startswith("cd "):
+                directory = command[3:]
+                try:
+                    os.chdir(directory)
+                except:
+                    print("The system cannot find the path specified.")
+            else:
+                os.system(command)
+
+            if ":" in command and command.index(":") > 0 and not "cd" in command:
+                print("To \'CD\' into a drive, please use \'CD DRIVE:\\\' instead.")
+
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt..\n")
+            break
 
 command_line()
